4233af85bd94030f268d94c24f5e809e67abfc59
kursun12 <salih_kara@hotmail.nl>
2019-10-16 00:04:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-is-fun
+ 
---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="Nav-Bar">
+          <p>item 1</p>
+          <p>item 2</p>
+          <p>item 3</p>
+          <p>item 4</p>
+        </div>
+        </header>
+
+        <body>         
+          <div className="Articles">
+            <div className="Article-Box">
+              <div>
+                <img src="/juice.jpg" alt="Avatar" className="card-image" />
+                <h2>Section1</h2>
+                <p>Dit is een opvuller text voor dit artikel</p>
+              </div>
+              <div>
+                <img src="/juice.jpg" alt="Avatar" className="card-image" />
+                <h2>Section2</h2>
+                <p>Dit is een opvuller text voor dit artikel</p>
+              </div>
+              <div>
+                <img src="/juice.jpg" alt="Avatar" className="card-image" />
+                <h2>Section3</h2>
+                <p>Dit is een opvuller text voor dit artikel</p>
+              </div>
+              <div>
+                <img src="/juice.jpg" alt="Avatar" className="card-image" />
+                <h2>Section4</h2>
+                <p>Dit is een opvuller text voor dit artikel</p>
+              </div>
+              <div>
+                <img src="/juice.jpg" alt="Avatar" className="card-image" />
+                <h2>Section3</h2>
+                <p>Dit is een opvuller text voor dit artikel</p>
+              </div>
+              <div>
+                <img src="/juice.jpg" alt="Avatar" className="card-image" />
+                <h2>Section4</h2>
+                <p>Dit is een opvuller text voor dit artikel</p>
+              </div>
+              <div>
+                <img src="/juice.jpg" alt="Avatar" className="card-image" />
+                <h2>Section3</h2>
+                <p>Dit is een opvuller text voor dit artikel</p>
+              </div>
+              <div>
+                <img src="/juice.jpg" alt="Avatar" className="card-image" />
+                <h2>Section4</h2>
+                <p>Dit is een opvuller text voor dit artikel</p>
+              </div>
+            </div>
+          </div>
+
+          <footer>
+          <p>disclaimer fam: </p>
+          </footer>
+          
+        </body>
+    </div>
+  );
+}
+
+export default App;
