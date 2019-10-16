34b2f89d0df100f6ef4b9ad52db9df3d3ad56b79
SaliihK-PC\SaliihK <salih_kara@hotmail.nl>
2019-10-16 10:34:20 +0200

    Router

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import About from './Componenets/About'
+
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { Link} from 'react-router-dom';
+
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
         <div className="Nav-Bar">
-          <p>item 1</p>
+          <Link to = "/About">About</Link>
+          {/* <p>About</p> */}
           <p>item 2</p>
           <p>item 3</p>
           <p>item 4</p>
         </div>
         </header>
-
+        <Switch>
+          {/* <Route exact path="/" component={}></Route> */}
+          <Route exact path="/About" component={About}></Route>
+        </Switch>
         <body>         
           <div className="Articles">
             <div className="Article-Box">
@@ -66,6 +76,7 @@ function App() {
           
         </body>
     </div>
+    </Router>
   );
 }
 
